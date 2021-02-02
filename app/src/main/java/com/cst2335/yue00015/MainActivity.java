d4ee8e6575f7c7af584dc6ecac6feac6271a68ac
HelloNYAC <77427085+HelloNYAC@users.noreply.github.com>
2021-02-01 23:19:28 -0500

    Step3 Lab2

--- app/src/main/java/com/cst2335/yue00015/MainActivity.java
+++ app/src/main/java/com/cst2335/yue00015/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_grid);
+        setContentView(R.layout.activity_main_relative);
 
         clickBtn = (Button)findViewById(R.id.btn_1);
         clickBtn.setOnClickListener((v) -> {
