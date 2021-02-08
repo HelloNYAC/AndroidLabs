c64e2fe110666f1082772f8911fb07619c3592a4
HelloNYAC <77427085+HelloNYAC@users.noreply.github.com>
2021-02-07 19:43:20 -0500

    2nd commit

--- app/src/main/java/com/cst2335/yue00015/MainActivity.java
+++ app/src/main/java/com/cst2335/yue00015/MainActivity.java
@@ -19,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
