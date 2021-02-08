9f4332c909aed09bb0f6b99a26403fefa87fb5e9
HelloNYAC <77427085+HelloNYAC@users.noreply.github.com>
2021-02-07 23:55:08 -0500

    added 2nd activity

--- app/src/main/java/com/cst2335/yue00015/MainActivity.java
+++ app/src/main/java/com/cst2335/yue00015/MainActivity.java
@@ -1,8 +1,12 @@
 package com.cst2335.yue00015;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
@@ -13,16 +17,27 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_profile);
+//        getSharedPreferences("name", 20);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+//        getSharedPreferences
+    }
+
+    //    Button secondButton = findViewById(R.id.loginbutton);
+//    Intent nextpage = new Intent(this, SecondActivity.clas);
+//
+//    secondButton.setOnClickListener(click -> startActivity(nextpage));
+
 }
---
+++ app/src/main/java/com/cst2335/yue00015/ProfileActivity.java
@@ -0,0 +1,50 @@
+package com.cst2335.yue00015;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class ProfileActivity extends AppCompatActivity {
+
+
+    private ImageButton mImageButton = findViewById(R.id.imgBtn);
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent nextPage = new Intent(this, MainActivity.class);
+        mImageButton.setOnClickListener(click -> startActivity(nextPage));
+    }
+
+    private void dispatchTakePictureIntent() {
+        android.content.Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageButton.setImageBitmap(imageBitmap);
+        }
+    }
+
+}
