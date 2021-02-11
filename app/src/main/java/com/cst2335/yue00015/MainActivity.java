660c7676603346c5312952de190738829a058ca3
HelloNYAC <77427085+HelloNYAC@users.noreply.github.com>
2021-02-11 14:48:27 -0500

    Finished lab 3

--- app/src/main/java/com/cst2335/yue00015/MainActivity.java
+++ app/src/main/java/com/cst2335/yue00015/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
@@ -19,21 +22,26 @@ import android.widget.Toast;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    private SharedPreferences pref;
+    private EditText inputEmail;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button loginButton = findViewById(R.id.loginbutton);
-        EditText inputEmail = findViewById(R.id.Email_ph);
-        EditText inputPasswd = findViewById(R.id.Password_ph);
 
-        Intent nextPage = new Intent(this, ProfileActivity.class);
+        loginButton = findViewById(R.id.loginbutton);
+        inputEmail = findViewById(R.id.Email_ph);
+        pref = getSharedPreferences("email", Context.MODE_PRIVATE);
+        String emaildef = pref.getString("email", "");
+        inputEmail.setText(emaildef);
+
+        Intent goToProfile = new Intent(MainActivity.this, ProfileActivity.class);
         loginButton.setOnClickListener(click ->
         {
-            nextPage.putExtra("typed", inputEmail.getText());
-            nextPage.putExtra("typed", inputPasswd.getText());
-            startActivity(nextPage);
+            goToProfile.putExtra("email", inputEmail.getText().toString());
+            startActivity(goToProfile);
         });
     }
 
@@ -42,4 +50,16 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        pref = getSharedPreferences("email", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editEmail = pref.edit();
+        String emailText = inputEmail.getText().toString();
+        editEmail.putString("email", emailText);
+        editEmail.commit();
+    }
+
+
 }
+
--- app/src/main/java/com/cst2335/yue00015/ProfileActivity.java
+++ app/src/main/java/com/cst2335/yue00015/ProfileActivity.java
@@ -3,24 +3,36 @@ package com.cst2335.yue00015;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class ProfileActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    public static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
+    private ImageButton mImageButton;
+    private EditText email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.e(ACTIVITY_NAME, "In function" + "onCreate");
         setContentView(R.layout.activity_profile);
-//        SharedPreferences prefs = getSharedPreferences(String email, String password)
+        mImageButton = findViewById(R.id.imgBtn);
+        email = findViewById(R.id.enter_email);
 
+        mImageButton.setOnClickListener(click -> dispatchTakePictureIntent());
+
+        Intent fromMain = getIntent();
+        email.setText(fromMain.getStringExtra("email"));
     }
 
     private void dispatchTakePictureIntent() {
@@ -33,11 +45,42 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        ImageButton mImageButton = findViewById(R.id.imgBtn);
+        mImageButton = findViewById(R.id.imgBtn);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             mImageButton.setImageBitmap(imageBitmap);
         }
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(ACTIVITY_NAME, "In function: " + "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(ACTIVITY_NAME, "In function: " + "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(ACTIVITY_NAME, "In function: " + "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(ACTIVITY_NAME, "In function: " + "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(ACTIVITY_NAME, "In function: " + "onDestroy");
+    }
+
 }
