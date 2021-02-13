06697776f27ac482c1afe2aaf7d702a385c30a62
HelloNYAC <77427085+HelloNYAC@users.noreply.github.com>
2021-02-13 00:31:44 -0500

    Added Typed msg

--- app/src/main/java/com/cst2335/yue00015/ChatRoomActivity.java
+++ app/src/main/java/com/cst2335/yue00015/ChatRoomActivity.java
@@ -3,12 +3,74 @@ package com.cst2335.yue00015;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Message;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ChatRoomActivity extends AppCompatActivity {
+    private ArrayList<String> list = new ArrayList<>();
+    private ChatRoomActivity.MyListAdapter myAdapter;
+    private ListView myList;
+    private Button send;
+    private Button receive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_room);
+
+        myAdapter = new MyListAdapter();
+        myList = (ListView)findViewById(R.id.thelistview);
+        myList.setAdapter(myAdapter);
+
+        EditText typed = findViewById(R.id.typeline);
+
+        send = findViewById(R.id.sendbtn);
+        send.setOnClickListener(click ->{
+            String sendText = typed.getText().toString();
+            list.add(sendText);
+            myAdapter.notifyDataSetChanged();
+        });
+
+//        receive = findViewById(R.id.rcvbtn);
+//        receive.setOnClickListener(click ->{
+//            list.add("Hiii");
+//            myAdapter.notifyDataSetChanged();
+//        });
+    }
+
+//    class Message
+    class MyListAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return list.get(position);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = getLayoutInflater();
+            View thisRow = inflater.inflate(R.layout.row_send_layout, parent, false);
+            TextView thisRowText = thisRow.findViewById(R.id.row_item);
+            thisRowText.setText(getItem(position).toString());
+            return thisRow;
+            }
+
+        @Override
+        public long getItemId(int position) {
+            return (long) position;
+        }
     }
 }
--- app/src/main/java/com/cst2335/yue00015/ProfileActivity.java
+++ app/src/main/java/com/cst2335/yue00015/ProfileActivity.java
@@ -20,20 +20,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ProfileActivity extends AppCompatActivity {
-
-
     static final int REQUEST_IMAGE_CAPTURE = 1;
     public static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
     private ImageButton mImageButton;
     private EditText emailText;
-    private ArrayList<String> list = new ArrayList<>(Arrays.asList());
-//    private MyListAdapter myAdapter;
-    private ListView myList;
     private Button chat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.e(ACTIVITY_NAME, "In function" + "onCreate");
+
         setContentView(R.layout.activity_profile);
 
         chat = findViewById(R.id.chatbutton);
@@ -41,16 +38,15 @@ public class ProfileActivity extends AppCompatActivity {
         chat.setOnClickListener(click -> {
             startActivity(GoToChat);
         });
-        Log.e(ACTIVITY_NAME, "In function" + "onCreate");
-        setContentView(R.layout.activity_profile);
+
         mImageButton = findViewById(R.id.imgBtn);
         emailText = findViewById(R.id.enter_email);
-
         mImageButton.setOnClickListener(click -> dispatchTakePictureIntent());
 
         Intent fromMain = getIntent();
         emailText.setText(fromMain.getStringExtra("email"));
     }
+
 
     private void dispatchTakePictureIntent() {
         android.content.Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -70,64 +66,37 @@ public class ProfileActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.e(ACTIVITY_NAME, "In function: " + "onStart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.e(ACTIVITY_NAME, "In function: " + "onResume");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.e(ACTIVITY_NAME, "In function: " + "onPause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.e(ACTIVITY_NAME, "In function: " + "onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.e(ACTIVITY_NAME, "In function: " + "onDestroy");
-    }
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        Log.e(ACTIVITY_NAME, "In function: " + "onStart");
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        Log.e(ACTIVITY_NAME, "In function: " + "onResume");
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        Log.e(ACTIVITY_NAME, "In function: " + "onPause");
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        Log.e(ACTIVITY_NAME, "In function: " + "onStop");
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        Log.e(ACTIVITY_NAME, "In function: " + "onDestroy");
+//    }
 
 }
 
 
-//        myList = findViewById(R.id.list_item);
-//        myList.setAdapter(myAdapter);
-        }
 
-//        class MyListAdapter extends BaseAdapter {
-//            @Override
-//            public int getCount() {
-//                return list.size();
-//            }
-//
-//            @Override
-//            public Object getItem(int position) {
-//                return list.get(position);
-//            }
-//            @Override
-//            public View getView(int position, View convertView, ViewGroup parent) {
-//                return null;
-//            }
-//
-//            @Override
-//            public long getItemId(int position) {
-//                return position;
-//            }
-//
-//
-//        }
-
-//    }
